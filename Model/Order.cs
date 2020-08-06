6e52cc048f5fab636bf61d69b2650260bb865ead
Neha Kumari <kneha2429@gmail.com>
2020-08-06 06:07:17 +0530

    Base Code

---
+++ Model/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model
+{
+   public class Order
+    {
+        public int OrderId { get; set; }
+        public char ProductId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public char PromotionType { get; set; }
+
+
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model
+{
+    public class Product
+    {
+        public char ProductId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ProductName { get; set; }
+    }
+}
